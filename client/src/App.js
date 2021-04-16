f966a69dde70a3c409be630ce410fce7b7ac31eb
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-04-16 11:35:33 -0300

    modificando App.js, criando reduces

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+
 import memories from './images/memories.png';
 
 import Form from './components/Form/Form';
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 import Post from './Post/Post';
 
+import useStyles from './styles';
+
+
 const Posts = () =>{
+    const classes = useStyles();
+
     return(
         <>
-            <h1>Posts</h1>
+            <h1 className={classes.something} >Posts</h1>
             <Post/>
             <Post/>
         </>
